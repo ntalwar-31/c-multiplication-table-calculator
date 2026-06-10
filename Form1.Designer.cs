ef581d1c2a0382059206456c3fa6f08e96068f99
Nandika Talwar <ntalwar1999@gmail.com>
2026-06-10 15:34:19 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace MultiplicationTables
+{
+    partial class MultiplicationTables
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTableof = new System.Windows.Forms.Label();
+            this.lblFrom = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.txtStart = new System.Windows.Forms.TextBox();
+            this.txtFinish = new System.Windows.Forms.TextBox();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.chkReverse = new System.Windows.Forms.CheckBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTableof
+            // 
+            this.lblTableof.AutoSize = true;
+            this.lblTableof.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTableof.Location = new System.Drawing.Point(99, 64);
+            this.lblTableof.Name = "lblTableof";
+            this.lblTableof.Size = new System.Drawing.Size(227, 26);
+            this.lblTableof.TabIndex = 0;
+            this.lblTableof.Text = "Multiplication Table of:";
+            // 
+            // lblFrom
+            // 
+            this.lblFrom.AutoSize = true;
+            this.lblFrom.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFrom.Location = new System.Drawing.Point(99, 184);
+            this.lblFrom.Name = "lblFrom";
+            this.lblFrom.Size = new System.Drawing.Size(232, 26);
+            this.lblFrom.TabIndex = 1;
+            this.lblFrom.Text = "Showing from number:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(99, 313);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 26);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "To number:";
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumber.Location = new System.Drawing.Point(103, 105);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(176, 32);
+            this.txtNumber.TabIndex = 3;
+            // 
+            // txtStart
+            // 
+            this.txtStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStart.Location = new System.Drawing.Point(102, 228);
+            this.txtStart.Name = "txtStart";
+            this.txtStart.Size = new System.Drawing.Size(177, 32);
+            this.txtStart.TabIndex = 4;
+            // 
+            // txtFinish
+            // 
+            this.txtFinish.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFinish.Location = new System.Drawing.Point(102, 353);
+            this.txtFinish.Name = "txtFinish";
+            this.txtFinish.Size = new System.Drawing.Size(177, 32);
+            this.txtFinish.TabIndex = 5;
+            // 
+            // btnShow
+            // 
+            this.btnShow.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShow.Location = new System.Drawing.Point(102, 433);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(147, 46);
+            this.btnShow.TabIndex = 6;
+            this.btnShow.Text = "Show";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // chkReverse
+            // 
+            this.chkReverse.AutoSize = true;
+            this.chkReverse.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkReverse.Location = new System.Drawing.Point(103, 517);
+            this.chkReverse.Name = "chkReverse";
+            this.chkReverse.Size = new System.Drawing.Size(119, 30);
+            this.chkReverse.TabIndex = 7;
+            this.chkReverse.Text = "Reverse";
+            this.chkReverse.UseVisualStyleBackColor = true;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(484, 64);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOutput.Size = new System.Drawing.Size(318, 486);
+            this.txtOutput.TabIndex = 8;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(291, 435);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(146, 46);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // MultiplicationTables
+            // 
+            this.AcceptButton = this.btnShow;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(904, 613);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.chkReverse);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.txtFinish);
+            this.Controls.Add(this.txtStart);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblFrom);
+            this.Controls.Add(this.lblTableof);
+            this.Name = "MultiplicationTables";
+            this.Text = "Multiplication Tables";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTableof;
+        private System.Windows.Forms.Label lblFrom;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.TextBox txtStart;
+        private System.Windows.Forms.TextBox txtFinish;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.CheckBox chkReverse;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiplicationTables
+{
+    public partial class MultiplicationTables : Form
+    {
+        public MultiplicationTables()
+        {
+            InitializeComponent();
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            // clearing any previous output
+            txtOutput.Clear();
+
+            // parse inputs
+            if (int.TryParse(txtNumber.Text, out int number) &&
+                int.TryParse(txtStart.Text, out int start) &&
+                int.TryParse(txtFinish.Text, out int finish))
+            {
+                // ensure start <= finish
+                if (start > finish)
+                {
+                    int temp = start;
+                    start = finish;
+                    finish = temp;
+                }
+
+                // produce multiplication lines using the loop variable for the left-hand operand
+                if (chkReverse.Checked)
+                {
+                    for (int i = finish; i >= start; i--)
+                    {
+                        txtOutput.AppendText($"{i} x {number} = {i * number}{Environment.NewLine}");
+                    }
+                }
+                else
+                {
+                    for (int i = start; i <= finish; i++)
+                    {
+                        txtOutput.AppendText($"{i} x {number} = {i * number}{Environment.NewLine}");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter valid integers for Number, Start and Finish.");
+            }
+        }
+    }
+}
+
